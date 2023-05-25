847815882ae524a2dd50052c21dd43fe4d0bf957
GonFreecs10 <110759289+GonFreecs10@users.noreply.github.com>
2023-05-25 10:27:12 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:hi_hi/start_screen.dart';
+
+void main(List<String> args) {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
